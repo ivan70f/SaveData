0d0d3bf3115840924bc4b55278311d30196a7b8c
Ivan Fedorchenko <65971171+ivan70f@users.noreply.github.com>
2020-09-13 07:07:29 +0300

    Init

---
+++ Core/DataLoader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace SaveData.Core
+{
+    public static class DataLoader
+    {
+        public static T GetData<T>(string _saveFileName)
+        {
+            if (TryLoadData(_saveFileName, out T _data))
+                return _data;
+
+            T _newData = Activator.CreateInstance<T>();
+            DataSaver.SaveData(_newData);
+            return _newData;
+        }
+
+        public static T GetData<T>()
+        {
+            if (TryLoadData(typeof(T).Name, out T _data))
+                return _data;
+
+            T _newData = Activator.CreateInstance<T>();
+            DataSaver.SaveData(_newData);
+            return _newData;
+        }
+
+        private static bool TryLoadData<T>(string _name, out T _data)
+        {
+            string _path = Application.persistentDataPath + _name;
+
+            _data = default;
+
+            if (File.Exists(_path) == false)
+                return false;
+
+            BinaryFormatter _binaryFormatter = new BinaryFormatter();
+            FileStream _stream = new FileStream(_path, FileMode.Open);
+            _data = (T) _binaryFormatter.Deserialize(_stream);
+            _stream.Close();
+
+            return true;
+        }
+    }
+}
---
+++ Example/Example.cs
@@ -0,0 +1,19 @@
+﻿using SaveData.Core;
+using UnityEngine;
+
+namespace SaveData.Example
+{
+    public class Example : MonoBehaviour
+    {
+        private void Start()
+        {
+            CheckData _data = DataLoader.GetData<CheckData>();
+
+            Debug.Log(_data.a);
+            
+            _data.a++;
+
+            DataSaver.SaveData(_data);
+        }
+    }
+}
